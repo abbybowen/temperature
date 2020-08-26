03d4f057a44ff9431f9b982e2a100cedbabc33ff
abbybowen <aeb6095@psu.edu>
2020-08-26 23:22:38 +0000

    I added lines for clarity.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 temperature = input("Enter temperature in celsius: ")
 temperature = float(temperature)
 f = (temperature * 9/5) + 32
+#print(str(f))
+#print(str(temperature))
 print(str(temperature) + " Celsius is equivalent to " + str(f) + " Fahrenheit")
 
 
