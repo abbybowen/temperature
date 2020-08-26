f153a4a6b0947f3dfb428c7a19fad3de005a7be7
abbybowen <aeb6095@psu.edu>
2020-08-26 23:24:17 +0000

    Put in degree symbols.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ temperature = float(temperature)
 f = (temperature * 9/5) + 32
 #print(str(f))
 #print(str(temperature))
-print(str(temperature) + " Celsius is equivalent to " + str(f) + " Fahrenheit")
+print(str(temperature) + chr(186) +" Celsius is equivalent to " + str(f) + chr(186) + " Fahrenheit")
 
 
 
