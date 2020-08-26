e8313b34ca378963c1e6df2db6f87b8c906812b6
abbybowen <aeb6095@psu.edu>
2020-08-26 23:29:15 +0000

    symbol

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ temperature = float(temperature)
 f = (temperature * 9/5) + 32
 #print(str(f))
 #print(str(temperature))
-print(str(temperature) + chr(186) +" Celsius is equivalent to " + str(f) + chr(186) + " Fahrenheit")
+print(str(temperature) + chr(248) +" in Celsius is equivalent to " + str(f) + chr(186) + " Fahrenheit")
 
 
 
