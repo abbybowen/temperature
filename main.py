ae48c400e0e5a70e76f902f727cf104374417eb6
abbybowen <aeb6095@psu.edu>
2020-08-26 23:15:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+temperature = input("Enter temperature in celsius: ")
+temperature = float(temperature)
+f = (temperature * 9/5) + 32
+print(str(temperature) + " Celsius is equivalent to " + str(f) + " Fahrenheit")
+
+
+
+
+
